84a8fad3426bb5128549f12a381f4895a93f8ad4
mostafa mohammadi <39732739+mostafa001994@users.noreply.github.com>
2026-08-19 09:01:40 +0330

    update Meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Binex | مدیریت هوشمند با هوش مصنوعی",
+  title: "Binix | مدیریت هوشمند با هوش مصنوعی",
   description:
-    "Binex؛ ارائه‌دهنده راهکارهای مدیریت، اتوماسیون و هوش مصنوعی برای کسب‌وکارها",
+    "Binix؛ ارائه‌دهنده راهکارهای مدیریت، اتوماسیون و هوش مصنوعی برای کسب‌وکارها",
 };
 
 export default function RootLayout({
